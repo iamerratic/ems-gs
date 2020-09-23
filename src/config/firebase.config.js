30b7e097acae2911daec6334e2dcc62062465eff
Keshav Singh Yadav <keshavsinghyadav@Keshavs-MacBook-Pro.local>
2020-09-23 12:52:30 +0530

    all done

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,14 +1,6 @@
 import * as firebase from 'firebase';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDaoyxR1IVVZBCJ2ICoZLumiTDIK83vDlc",
-    authDomain: "ems-gs.firebaseapp.com",
-    databaseURL: "https://ems-gs.firebaseio.com",
-    projectId: "ems-gs",
-    storageBucket: "ems-gs.appspot.com",
-    messagingSenderId: "1050835199269",
-    appId: "1:1050835199269:web:c976bb358e2563fbf3e6cc"
-};
+var firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 
